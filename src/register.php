fa3af40227216b36a0bb6d258510a96de5808488
Luke Swanson <lwswanson2@wisc.edu>
2026-09-15 12:05:23 -0500

    Clean up spacing a bit.

--- src/register.php
+++ src/register.php
@@ -8,9 +8,9 @@ $conn = mysqli_connect($servername, $username, $password, $db);
 if(!$conn) die("Connection failed: " . mysqli_connect_error());
 
 //per dump
-if(isset($_REQUEST["class_id"]))          $class_id          = mysqli_real_escape_string($conn,$_REQUEST["class_id"]); else die("No class_id");
-if(isset($_REQUEST["username"]))          $player_username   = mysqli_real_escape_string($conn,$_REQUEST["username"]); else die("No username");
-if(isset($_REQUEST["player_id"]))         $player_id         = preg_replace("/[^a-zA-Z0-9]+/", "", $_REQUEST["player_id"]); else die("No player_id");
+if(isset($_REQUEST["class_id"]))  $class_id        = mysqli_real_escape_string($conn,$_REQUEST["class_id"]); else die("No class_id");
+if(isset($_REQUEST["username"]))  $player_username = mysqli_real_escape_string($conn,$_REQUEST["username"]); else die("No username");
+if(isset($_REQUEST["player_id"])) $player_id       = preg_replace("/[^a-zA-Z0-9]+/", "", $_REQUEST["player_id"]); else die("No player_id");
 
 $query = "INSERT INTO players (".
   "class_id,".
